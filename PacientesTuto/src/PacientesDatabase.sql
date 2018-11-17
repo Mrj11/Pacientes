ab4fefc9c0686e5fb772f906425f4059f8fb0491
alumno <alumno@913-m19.gabinete.frc.utn.edu.ar>
2018-11-16 21:29:44 -0300

    proyecto parte 1

---
+++ PacientesTuto/src/PacientesDatabase.sql
@@ -0,0 +1,100 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  alumno
+ * Created: 16-nov-2018
+ */
+
+create database pacientes;
+use pacientes;
+
+create table paciente (
+idpaciente int auto_increment,
+nombre varchar (40),
+apellido varchar (40),
+tipodoc int,
+nrodoc int,
+telefono int,
+email varchar (60),
+constraint pk_idpaciente primary key (idpaciente));
+
+create table provincia(
+idprovincia int,
+nombre varchar (50),
+constraint pk_idprov primary key (idprovincia)
+);  
+
+create table localidad(
+idlocalidad int,
+nombre varchar (50),
+fkprovincia int,
+constraint pk_idloc primary key (idlocalidad),
+constraint fk_provincia1 foreign key (fkprovincia) references provincia(idprovincia)
+);
+
+
+create table domicilio(
+id_domicilio int auto_increment,
+calle varchar (40),
+numero int,
+codpostal int,
+piso int,
+dpto int,
+fklocalidad int,
+fkprovincia int,
+fkidpaciente int,
+constraint pk_iddomicilio primary key (id_domicilio),
+constraint fk_idpac foreign key (fkidpaciente) references paciente(idpaciente),
+constraint fk_local foreign key (fklocalidad) references localidad(idlocalidad),
+constraint fk_prov foreign key (fkprovincia) references provincia(idprovincia)
+);
+
+insert into provincia
+values (1,'Buenos Aires');
+insert into provincia
+values (2, 'Catamarca');
+insert into provincia
+values (3,'Chaco');
+insert into provincia
+values (4, 'Chubut');
+insert into provincia
+values (5, 'Cordoba');
+insert into provincia
+values (6, 'Corrientes');
+insert into provincia
+values (7, 'Entre Rios');
+insert into provincia
+values (8, 'Formosa');
+insert into provincia
+values (9, 'Jujuy');
+insert into provincia
+values (10, 'La Pampa');
+insert into provincia
+values (11, 'La Rioja');
+insert into provincia
+values (12, 'Mendoza');
+insert into provincia
+values (13, 'Misiones');
+insert into provincia
+values (14, 'Neuquen');
+insert into provincia
+values (15, 'Rio Negro');
+insert into provincia
+values (16, 'Salta');
+insert into provincia
+values (17,'San Juan');
+insert into provincia
+values (18, 'San Luis');
+insert into provincia
+values (19, 'Santa Cruz');
+insert into provincia
+values (20, 'Santa Fe');
+insert into provincia
+values (21, 'Santiago del Estero');
+insert into provincia
+values (22, 'Tierra del Fuego');
+insert into provincia
+values (23, 'Tucuman');
---
+++ PacientesTuto/src/pacientestuto/domicilio.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pacientestuto;
+
+/**
+ *
+ * @author alumno
+ */
+public class domicilio {
+    private String calle;
+    private int numero;
+    private int codpostal;
+    private int piso;
+    private String dpto;
+    private String localidad;
+    private String provincia;
+    private String idpaciente;
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getCodpostal() {
+        return codpostal;
+    }
+
+    public void setCodpostal(int codpostal) {
+        this.codpostal = codpostal;
+    }
+
+    public int getPiso() {
+        return piso;
+    }
+
+    public void setPiso(int piso) {
+        this.piso = piso;
+    }
+
+    public String getDpto() {
+        return dpto;
+    }
+
+    public void setDpto(String dpto) {
+        this.dpto = dpto;
+    }
+
+    public String getLocalidad() {
+        return localidad;
+    }
+
+    public void setLocalidad(String localidad) {
+        this.localidad = localidad;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+
+    public String getIdpaciente() {
+        return idpaciente;
+    }
+
+    public void setIdpaciente(String idpaciente) {
+        this.idpaciente = idpaciente;
+    }
+    
+    
+}
